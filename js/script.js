0ebf082c8ada1de85638787eba92824d754c23b3
Tasha Romero <tasha.romero713@gmail.com>
2021-11-14 23:20:04 -0700

    Additional Navbars

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
-var x ="Hello World"
+// console.log(document.getElementById("title"));
+// console.log(document instanceof HTMLDocument);
+document.addEventListener("DOMContentLoaded",
+    function (event) {
+        function sayHello (event) {
+            console.log(event);
+            this.textContent = "Said it!";
+            var name = 
+                document.getElementById("name").value;
+                var message = "<h2>Hello " + name + "!</h2>";
+        
+                // document.getElementById("content").textContent =
+                // message;
+        
+                document.getElementById("content").innerHTML =
+                message;
+        
+                if (name === "student") {
+                    var title = 
+                        document
+                        .querySelector('#title')
+                        .textContent;
+                    title += "& Loving it!"
+                    document
+                    .querySelector('h1')
+                    .textContent = title;
+                }
+        }
+    }
+
+        document.querySelector("button")
+        .addEventListener("click", sayHello);
+);
+
+//Unobstrusive event binding
+
+
+// document.querySelector("button")
